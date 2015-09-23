2b71291809b158c5c2fefcd036d28928db4ebb54
Harm73 <harmbruinsma@home.nl>
2015-09-23 15:01:03 +0200

    Added: a agreement page

---
+++ client-side/C#/GlobeSpotterArcGISPro/GlobeSpotterArcGISPro/AddIns/Pages/Agreement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace GlobeSpotterArcGISPro.AddIns.Pages
+{
+  internal class Agreement: Page
+  {
+    protected Agreement()
+    {
+      // empty
+    }
+
+    public string AgreementText
+    {
+      get
+      {
+        Type type = GetType();
+        Assembly assembly = type.Assembly;
+        const string agreementPath = "GlobeSpotterArcGISPro.Doc.Agreement.txt";
+        Stream agreementStream = assembly.GetManifestResourceStream(agreementPath);
+        string result = string.Empty;
+
+        if (agreementStream != null)
+        {
+          var reader = new StreamReader(agreementStream);
+          result = reader.ReadToEnd();
+          reader.Close();
+        }
+
+        return result;
+      }
+    }
+  }
+}
---
+++ client-side/C#/GlobeSpotterArcGISPro/GlobeSpotterArcGISPro/AddIns/Views/Agreement.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace GlobeSpotterArcGISPro.AddIns.Views
+{
+  /// <summary>
+  /// Interaction logic for Agreement.xaml
+  /// </summary>
+  public partial class Agreement
+  {
+    public Agreement()
+    {
+      InitializeComponent();
+    }
+  }
+}
